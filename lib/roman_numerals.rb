83ebe2769183541afda535c7ee148bb8c4d6d6f9
ajbater <amanda22@hotmail.co.uk>
2016-11-16 09:25:18 +0000

    works for 1, 5 and 10

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,7 +1,20 @@
 class RomanNumerals
 
-  def convert(number)
+  NUMERALS = {
+    10 => "X",
+    5 => "V",
+    1 => "I"
+  }
 
+  def convert(number)
+    roman_numeral = ""
+    NUMERALS.each do |key, value|
+      (number / key).times do
+        roman_numeral << value
+        number -= key
+      end
+    end
+    roman_numeral
   end
 
 end
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -1,9 +1,21 @@
 require 'roman_numerals'
 
 describe RomanNumerals do
-
   it {is_expected.to respond_to(:convert)}
 
   it {is_expected.to respond_to(:convert).with(1).argument}
 
+  describe '#convert' do
+    it 'returns "I" when passed 1' do
+      expect(subject.convert(1)).to eq "I"
+    end
+
+    it 'returns "V" when passed 5' do
+      expect(subject.convert(5)).to eq "V"
+    end
+
+    it 'returns "X" when passed 10' do
+      expect(subject.convert(10)).to eq "X"
+    end
+  end
 end
